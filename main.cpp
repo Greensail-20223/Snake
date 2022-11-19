d3be2f401506818501d584769099f1ab1609c3b3
ekaterina.deulina <ekaterina.deulina12268@gmail.com>
2022-11-19 16:26:10 +0300

    updatedVersion

--- main.cpp
+++ main.cpp
@@ -2,21 +2,8 @@
 #include "vars.h"
 #include "game_mathods.h"
 
-std::vector<std::string> graphics_menu_items = {"Low graphics", "High graphics", "Quit", "Choose graphics"};
-
-int graphics_color = 0;
-bool graphics_pause;
-
-void set_sound(){
-    enter_buffer.loadFromFile("sounds/enter.wav");
-    enter_sound.setBuffer(enter_buffer);
-    menu_buffer.loadFromFile("sounds/menu.wav");
-    menu_sound.setBuffer(menu_buffer);
-}
-
 int main()
 {
-    set_sound();
     game();
 
 
